56972c9651efb52a6aada9668afffe9cafe8a7eb
Mateus Braz <silvabraz96@gmail.com>
2021-08-02 01:05:36 -0300

    Tratando exceptions

---
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -0,0 +1,22 @@
+package com.algaworks.algafood.api.exceptionhandler;
+
+import lombok.Getter;
+
+@Getter
+public enum ProblemType {
+
+    MENSAGEM_INCOMPREENSIVEL("Mensagem incompreensível", "/mensagem-incompreensivel"),
+    RECURSO_NAO_ENCONTRADO("Recurso não encontrado", "/recurso-nao-encontrado"),
+    ENTIDADE_EM_USO("Entidade em uso", "/entidade-em-uso"),
+    ERRO_NEGOCIO("Violação de regra de negócio", "/erro-negocio"),
+    PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
+    ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema");
+
+    private String title;
+    private String uri;
+
+    ProblemType(String title, String path) {
+        this.title = title;
+        this.uri = "https://algafood.com.br" + path;
+    }
+}
